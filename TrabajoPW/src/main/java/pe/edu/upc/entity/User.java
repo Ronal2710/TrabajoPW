47eebf50818b43bb665ca405e24444770a4eaf0e
Ronal Inga <Ronal Inga@DESKTOP-14B9M9S>
2020-05-19 20:01:02 -0500

    RONAL

---
+++ TrabajoPW/src/main/java/pe/edu/upc/entity/User.java
@@ -0,0 +1,82 @@
+package pe.edu.upc.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int id;
+
+
+	@MapsId
+	@Column(name = "username", nullable = false, length = 30)
+	private String username;
+
+	@Column(name = "password", nullable = false, length = 80)
+	private String password;
+
+	@Column(name = "state", nullable = false, length = 1)
+	private String state;
+
+	
+	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(int id, String username, String password, String state) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.state = state;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	
+
+}
+
+
