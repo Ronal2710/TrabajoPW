9eebd74df28fa54e8bf721140f273fde74ca5d84
Ronal Inga <Ronal Inga@DESKTOP-14B9M9S>
2020-06-16 21:14:47 -0500

    ROL Y USER - GABRIEL MIRANDA

--- TrabajoPW/src/main/java/pe/edu/upc/entity/Rol.java
+++ TrabajoPW/src/main/java/pe/edu/upc/entity/Rol.java
@@ -2,6 +2,7 @@ package pe.edu.upc.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,34 +13,37 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table(name = "roles", uniqueConstraints = { @UniqueConstraint(columnNames = { "user_id", "rol" }) })
+@Table(name = "roles", uniqueConstraints = { @UniqueConstraint(columnNames = { "user_id", "rolname" }) })
 public class Rol implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-
-	private String rol;
+	private Long idrol;
+    @Column(name="rolname")
+	private String rolname;
 	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
 
-	public Long getId() {
-		return id;
+
+
+	
+	public Long getIdrol() {
+		return idrol;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	public void setIdrol(Long idrol) {
+		this.idrol = idrol;
 	}
 
-	public String getRol() {
-		return rol;
+	public String getRolname() {
+		return rolname;
 	}
 
-	public void setRol(String rol) {
-		this.rol = rol;
+	public void setRolname(String rolname) {
+		this.rolname = rolname;
 	}
 
 	public User getUser() {
